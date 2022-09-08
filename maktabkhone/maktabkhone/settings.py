09bb6d8db19dd9eb96acd4a1524f44919cd82326
MjHashemi <61750073+MjHashemi@users.noreply.github.com>
2022-09-07 21:29:32 -0700

    Update settings.py

--- maktabkhone/maktabkhone/settings.py
+++ maktabkhone/maktabkhone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ex1_zo9iu!i29-o71&e0@(9g150*ty-y*-#ocf#l25h0wpjxf)'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
